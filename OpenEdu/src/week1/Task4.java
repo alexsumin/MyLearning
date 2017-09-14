be788c841b82997e99bef813784d8430fba2b922
alexsumin <sumin.alexandr@gmail.com>
2017-09-14 12:49:54 +0300

    task 4 complete

--- OpenEdu/src/week1/Task4.java
+++ OpenEdu/src/week1/Task4.java
@@ -9,29 +9,45 @@ public class Task4 {
 
             int size = in.nextInt();
             double element;
-            double array[] = new double[size];
-            Pair results[] = new Pair[size];
-
+            Pair array[] = new Pair[size];
 
             for (int i = 0; i < size; i++) {
                 element = in.nextDouble();
-                array[i] = element;
+                array[i] = new Pair(element, i + 1);
             }
 
 
-            for ()
-
+            for (int i = 0; i < size - 1; i++) {
+                for (int j = size - 1; j > i; j--) {
+                    if (array[j].get() < array[j - 1].get()) {
+                        Pair temp = array[j];
+                        array[j] = array[j - 1];
+                        array[j - 1] = temp;
+                    }
+                }
+            }
+            out.print(array[0].getIndex() + " " + array[size / 2].getIndex() + " " +
+                    array[size - 1].getIndex());
         }
     }
 
-    class Pair {
-        double value;
-        int index;
 
-        Pair(double value, int index) {
-            this.value = value;
-            this.index = index;
-        }
+}
 
+class Pair {
+    double value;
+    int index;
+
+    Pair(double value, int index) {
+        this.value = value;
+        this.index = index;
+    }
+
+    double get() {
+        return value;
+    }
+
+    int getIndex() {
+        return index;
     }
 }
